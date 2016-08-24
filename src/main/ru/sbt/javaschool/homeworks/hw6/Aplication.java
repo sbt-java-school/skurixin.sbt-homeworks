1e4796f14377e750e5a1a5134aab8f58eeb70c9d
Sergey <sergeySkurixin@bk.ru>
2016-08-24 15:15:03 +0700

    Homework 6, added checkConstants

--- src/main/ru/sbt/javaschool/homeworks/hw6/Aplication.java
+++ src/main/ru/sbt/javaschool/homeworks/hw6/Aplication.java
@@ -1,27 +1,42 @@
 package ru.sbt.javaschool.homeworks.hw6;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 /**
  * Created by скурихин on 24.08.2016.
  */
 public class Aplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException {
         TestClass testClass = new TestClass();
-        printAllMethods(testClass.getClass());
-        printAllGetters(testClass.getClass());
-        checkConstants(testClass.getClass());
+//        printAllMethods(testClass.getClass());
+//        printAllGetters(testClass.getClass());
+        checkConstants(testClass);
     }
 
-    private static void checkConstants(Class<? extends TestClass> aClass) {
-        //добавить проверку
+    private static void checkConstants(TestClass object) throws IllegalAccessException, NoSuchFieldException {
+        Class<? extends TestClass> aClass = object.getClass();
+        System.out.println("Check all Constants of class " + aClass.getName());
+        int aClassModifiers = aClass.getModifiers();
+        Field[] declaredFields = aClass.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            int declaredFieldModifiers = declaredField.getModifiers();
+            declaredField.setAccessible(true);
+            if (Modifier.isFinal(declaredFieldModifiers) && Modifier.isStatic(declaredFieldModifiers)
+                    && declaredField.getType().getName().equals(String.class.getName())) {
+                if (!declaredField.getName().equals(declaredField.get(object))) {
+                    System.out.println("Name of field " + declaredField.getName() + "hasnt coincided with value");
+                }
+            }
+        }
     }
 
     private static void printAllGetters(Class<? extends TestClass> aClass) {
         System.out.println("All getters of a class " + aClass.getName());
         Method[] methods = aClass.getMethods();
         for (Method method : methods) {
-            if(method.getName().contains("get")){
+            if (method.getName().contains("get")) {
                 System.out.println(method);
             }
         }
